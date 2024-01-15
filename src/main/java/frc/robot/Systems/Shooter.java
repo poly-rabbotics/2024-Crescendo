a08610d67c1d2f302f410d9fa4f49a5276b0993a
JustBeChill <63560117+JustBeChill@users.noreply.github.com>
2024-01-15 00:20:52 -0800

    Persona 5 soundtrack is goated i wish the game would go on sale

--- src/main/java/frc/robot/Systems/Shooter.java
+++ src/main/java/frc/robot/Systems/Shooter.java
@@ -1,34 +1,40 @@
 package frc.robot.systems;
 
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.ControlModeValue;
-
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
-import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.hardware.TalonFX;
 
+import frc.robot.SmartPrintable;
 
-public class Shooter {
+public class Shooter extends SmartPrintable {
+
+    public enum ShooterState {
+        IDLE, RUNNING
+    }
     
     private static final Shooter instance = new Shooter();
+
+    private static final int SHOOTER_MOTOR_ID = 6;
+    private static final int RAMPING_THRESHOLD = 95; //Threshold where state goes from RAMPING to READY (when i implement it) in percent(?)
+
+    private static double velocity = 105; //Velocity in RPM
+    private static ShooterState currentState = ShooterState.IDLE;
 
     private static TalonFX shooterMotor;
     private static Slot0Configs slot0Configs;
     private static VelocityVoltage request;
-    private static boolean stopped = true;
-
+    
     private static final double P_0 = 0.5;
     private static final double I_0 = 0;
     private static final double D_0 = 0;
     private static final double S_0 = 0.05;
 
-    private static double velocity = 70; //Velocity in RPM
-
-    
     private Shooter() {
+        super();
+
         slot0Configs = new Slot0Configs();
-        shooterMotor = new TalonFX(6);
+        shooterMotor = new TalonFX(SHOOTER_MOTOR_ID);
 
         slot0Configs.kP = P_0;
         slot0Configs.kI = I_0;
@@ -39,7 +45,32 @@ public class Shooter {
         request = new VelocityVoltage(0).withSlot(0);
     }
 
-    public static void run(boolean dPadUp, boolean dPadDown, boolean resume, boolean stop) {
+    /**
+     * Runs the shooter. What, did you expect it to make you a sandwich?
+     * @param runShooter Hold the button to run the shooter
+     */
+    public static void run(boolean runShooter) {
+        if(runShooter) {
+            currentState = ShooterState.RUNNING;
+        } else {
+            currentState = ShooterState.IDLE;
+        }
+
+        if(currentState.equals(ShooterState.RUNNING)) {
+            shooterMotor.setControl(request.withVelocity(velocity));
+        } else {
+            shooterMotor.set(0);
+        }
+    }
+
+    /**
+     * Old and kind of jank run method that lets you change velocity & toggle running
+     * @param dPadUp Increments the velocity up by 5
+     * @param dPadDown Increments the velocity down by 5
+     * @param resume Toggles the shooter on
+     * @param stop Toggles the shooter off
+     */
+    public static void runTest(boolean dPadUp, boolean dPadDown, boolean resume, boolean stop) {
 
         if(dPadUp) {
             velocity += 5;
@@ -48,15 +79,29 @@ public class Shooter {
         }
 
         if(resume) {
-            stopped = false;
+            currentState = ShooterState.RUNNING;
         } else if(stop) {
-            stopped = true;
+            currentState = ShooterState.IDLE;
         }
 
         
-        if(!stopped) shooterMotor.setControl(request.withVelocity(velocity));
-        else shooterMotor.set(0);
+        if(currentState.equals(ShooterState.RUNNING)) {
+            shooterMotor.setControl(request.withVelocity(velocity));
+        } else {
+            shooterMotor.set(0);
+        }
+    }
 
+    /**
+     * Gets the current state of the shooter
+     * @return The current state of the shooter
+     */
+    public static ShooterState getState() {
+        return currentState;
+    }
+
+    public void print() {
+        SmartDashboard.putString("Shooter State", currentState.toString());
         SmartDashboard.putNumber("Target Velocity", velocity);
         SmartDashboard.putNumber("Motor Speed", shooterMotor.getVelocity().getValue());
         SmartDashboard.putNumber("Output Power", shooterMotor.get());
--- src/main/java/frc/robot/systems/Intake.java
+++ src/main/java/frc/robot/systems/Intake.java
@@ -1,21 +1,25 @@
 package frc.robot.systems;
 
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.ControlModeValue;
-
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.ctre.phoenix6.hardware.TalonFX;
 
-import com.ctre.phoenix6.configs.Slot0Configs;
-import com.ctre.phoenix6.controls.VelocityVoltage;
+import frc.robot.SmartPrintable;
 
 
-public class Intake {
+
+public class Intake extends SmartPrintable {
+
+    public enum IntakeState {
+        IDLE, INTAKING, OUTTAKING
+    }
     
     private static final Intake instance = new Intake();
 
+    private static IntakeState currentState = IntakeState.IDLE;
+
     private static TalonFX intakeMotor;
 
-    private static double velocity = 0.9; //Velocity in RPM
+    private static double intakeSpeed = 0.9; //intakeSpeed in RPM
     private static boolean stopped = true;
 
     
@@ -23,12 +27,34 @@ public class Intake {
         intakeMotor = new TalonFX(7);
     }
 
-    public static void run(boolean up, boolean down, boolean resume, boolean stop) {
+    public static void run(boolean intake, boolean outtake) {
+        if(intake) {
+            currentState = IntakeState.INTAKING;
+        } else if(outtake) {
+            currentState = IntakeState.OUTTAKING;
+        } else {
+            currentState = IntakeState.IDLE;
+        }
+
+        if(currentState.equals(IntakeState.INTAKING)) {
+            intakeMotor.set(intakeSpeed);
+        } else if(currentState.equals(IntakeState.OUTTAKING)) {
+            intakeMotor.set(-intakeSpeed);
+        } else {
+            intakeMotor.set(0);
+        }
+    }
+
+    /**
+     * More rudamentary method that allows for adjustment of the intake speed
+     * @param runIntake Hold the button to run the intake
+     */
+    public static void runTest(boolean up, boolean down, boolean resume, boolean stop) {
 
         if(up) {
-            velocity += 0.1;
+            intakeSpeed += 0.1;
         } else if(down) {
-            velocity -= 0.1;
+            intakeSpeed -= 0.1;
         }
 
         if(resume) {
@@ -38,10 +64,21 @@ public class Intake {
         }
 
         
-        if(!stopped) intakeMotor.set(velocity);
+        if(!stopped) intakeMotor.set(intakeSpeed);
         else intakeMotor.set(0);
 
-        SmartDashboard.putNumber("Target Velocity", velocity);
-        SmartDashboard.putNumber("Motor Speed", intakeMotor.getVelocity().getValue());
+    }
+
+    /**
+     * Returns the current state of the intake
+     * @return The current state of the intake
+     */
+    public static IntakeState getState() {
+        return currentState;
+    }
+
+    public void print() {
+        SmartDashboard.putNumber("Target Speed", intakeSpeed);
+        SmartDashboard.putNumber("Motor Speed", intakeMotor.get());
     }
 }
