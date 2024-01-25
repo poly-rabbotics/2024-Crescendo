41ca4d9707eee3ce3fe3ad2bf97a478c9f7b5e57
JustBeChill <kmalling16@gmail.com>
2024-01-25 14:57:50 -0800

    Intake stuff

--- src/main/java/frc/robot/systems/Intake.java
+++ src/main/java/frc/robot/systems/Intake.java
@@ -12,18 +12,31 @@ public class Intake extends SmartPrintable {
     private static final int INNER_MOTOR_PWM_CHANNEL = 1;
     private static final int OUTER_MOTOR_CAN_ID = 62;
 
+    private static final double INTAKE_SPEED = -0.35;
+    private static final double OUTTAKE_SPEED = 0.5;
+
     private final CANSparkMax outerMotor = new CANSparkMax(OUTER_MOTOR_CAN_ID, MotorType.kBrushless);
     private final VictorSP innerMotor = new VictorSP(INNER_MOTOR_PWM_CHANNEL);
 
     private static final Intake instance = new Intake();
 
+    private static double speed = 0;
+
     private Intake() {
         super();
     }
 
-    public static void run(double innerSpeed, double outerSpeed) {
-        instance.outerMotor.set(outerSpeed);
-        instance.innerMotor.set(innerSpeed);
+    public static void run(boolean intake, boolean outtake) {
+        if(intake) {
+            speed = INTAKE_SPEED;
+        } else if(outtake) {
+            speed = OUTTAKE_SPEED;
+        } else {
+            speed = 0;
+        }
+        
+        instance.outerMotor.set(speed);
+        instance.innerMotor.set(speed);
     }
 
     @Override
